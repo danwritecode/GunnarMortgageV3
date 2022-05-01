2b26641eecdd2d1a9657d9e47f1d64a53202fe8e
danwritecode <dnelson4993@outlook.com>
2022-04-30 23:15:26 -0400

    updated to only show arrows when applicable on timeline

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -53,22 +53,10 @@
           <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
         </svg>
       </button>
-
-      <button v-else disabled class="text-zinc-600">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
-        </svg>
-      </button>
     </div>
 
     <div class="hidden md:absolute w-full h-1/4 inset-x-0 bottom-0 md:flex items-center justify-center">
       <button v-if="curStepIndex + 1 < steps.length" @click="curStepIndex++" class="text-red-500 hover:text-red-600 transition-hover-300">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-        </svg>
-      </button>
-
-      <button v-else disabled class="text-zinc-600">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
         </svg>
