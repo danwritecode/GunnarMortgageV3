7da461668c173e375d3d1b4f58578e328fcd41f6
danwritecode <dnelson4993@outlook.com>
2022-05-11 19:48:29 -0400

    who is gunnar mobile formatting

--- components/index/whoisgunnar.vue
+++ components/index/whoisgunnar.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="relative">
-    <div class="absolute inset-x-0 bottom-0 h-1/2 bg-[url('~/assets/img/cartographer.webp')]"></div>
-    <div class="grid grid-cols-6 gap-56 min-h-[90vh] max-w-7xl lg:max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-start bg-white col-span-2">
-        <h1 class="left-8 top-10 font-black text-9xl text-white mix-blend-difference text-center max-w-xs">So who is Gunnar, really?</h1>
+  <div class="relative bg-white">
+    <div class="hidden lg:flex absolute inset-x-0 bottom-0 h-1/2 bg-[url('~/assets/img/cartographer.webp')]"></div>
+    <div class="grid grid-cols-1 lg:grid-cols-6 gap-10 min-h-[100vh] xl:min-h-[125vh] 2xl:min-h-[100vh] max-w-7xl lg:max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="relative lg:static flex items-center justify-center lg:justify-start bg-white lg:col-span-2 py-40 lg:py-0">
+        <div class="lg:hidden absolute inset-x-0 bottom-0 h-1/2 bg-[url('~/assets/img/cartographer.webp')] -mx-4 sm:-mx-6 lg:-mx-8"></div>
+        <h1 class="left-8 top-10 font-black text-7xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-white mix-blend-difference text-center max-w-xs">So who is Gunnar, really?</h1>
       </div>
-      <div class="col-span-4 grid grid-cols-1 pl-2">
-        <div class="relative flex items-center">
-          <div class="absolute grid grid-cols-6 gap-4 place-items-center">
-            <div class="relative bg-red-500 h-64 w-64 rounded-full col-span-2">
-              <img src="~/assets/img/GunnarHeadshot.jpg" alt="" class="absolute -left-2 -top-2 rounded-full h-64 w-64 object-cover">
+      <div class="grid grid-cols-1 gap-14 lg:gap-0 2xl:ml-16 lg:col-span-4">
+        <div class="relative lg:flex lg:items-center">
+          <div class="lg:absolute grid lg:grid-cols-6 gap-6 lg:gap-12 place-items-center">
+            <div class="relative bg-red-500 h-32 w-32 xl:h-56 xl:w-56 2xl:h-64 2xl:w-64 rounded-full col-span-2">
+              <img src="~/assets/img/GunnarHeadshot.jpg" alt="" class="absolute -left-2 -top-2 rounded-full h-32 w-32 xl:h-56 xl:w-56 2xl:h-64 2xl:w-64 object-cover">
             </div>
-            <dl class="mt-10 space-y-10 col-span-4">
+            <dl class="space-y-3 2xl:space-y-10 col-span-4">
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-red-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
+                    <div class="relative bg-red-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -24,16 +25,16 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
 
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-red-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
+                    <div class="relative bg-red-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -41,16 +42,16 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
 
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-red-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
+                    <div class="relative bg-red-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-zinc-800 text-red-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -58,24 +59,26 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-900">Something Professional</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
             </dl>
           </div>
         </div>
-        <div class="relative flex items-center">
-          <div class="absolute grid grid-cols-6 gap-4 place-items-center">
-            <div class="relative bg-turq-500 h-64 w-64 rounded-full col-span-2">
-              <img src="~/assets/img/GunFlamethrower.jpg" alt="" class="absolute -left-2 -top-2 rounded-full h-64 w-64 object-cover">
+
+        <div class="relative lg:flex lg:items-center pt-5">
+          <div class="lg:hidden absolute inset-x-0 bottom-0 h-full bg-[url('~/assets/img/cartographer.webp')] -mx-4 sm:-mx-6 lg:-mx-8"></div>
+          <div class="lg:absolute grid lg:grid-cols-6 gap-6 lg:gap-12 place-items-center">
+            <div class="relative bg-turq-500 h-32 w-32 xl:h-56 xl:w-56 2xl:h-64 2xl:w-64 rounded-full col-span-2">
+              <img src="~/assets/img/GunFlamethrower.jpg" alt="" class="absolute -left-2 -top-2 rounded-full h-32 w-32 xl:h-56 xl:w-56 2xl:h-64 2xl:w-64 object-cover">
             </div>
-            <dl class="mt-10 space-y-10 col-span-4">
+            <dl class="space-y-3 2xl:space-y-10 col-span-4">
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-turq-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
+                    <div class="relative bg-turq-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -83,16 +86,16 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
 
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-turq-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
+                    <div class="relative bg-turq-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -100,16 +103,16 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
 
               <div class="relative">
                 <dt>
                   <div class="absolute flex items-center justify-center pt-1">
-                    <div class="relative bg-turq-500 h-12 w-12 rounded-md">
-                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
+                    <div class="relative bg-turq-500 h-10 w-10 2xl:h-12 2xl:w-12 rounded-md">
+                      <div class="absolute flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-md bg-red-500 text-turq-500 left-1 bottom-1">
                         <!-- Heroicon name: outline/globe-alt -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -117,9 +120,9 @@
                       </div>
                     </div>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
+                  <p class="ml-16 2xl:text-lg leading-6 font-medium text-zinc-100">Something Cool</p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                <dd class="2xl:mt-2 ml-16 text-base text-zinc-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
               </div>
             </dl>
           </div>
