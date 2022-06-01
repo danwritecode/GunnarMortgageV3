dbecc472b2348b98ea65fcae5c3fc0a3dbed2f1a
danwritecode <dnelson4993@outlook.com>
2022-05-31 20:30:44 -0400

    integrated google reviews

--- components/index/reviews.vue
+++ components/index/reviews.vue
@@ -23,24 +23,24 @@
         </p>
       </div>
 
-      <div class="mt-10">
+      <div v-if="!loading" class="mt-10">
         <div class="relative h-96">
-          <div :key="n" v-for="n in [1,2,3,4,5]" class="absolute item mx-4 rounded-lg bg-white border border-gray-200 shadow-lg hover:shadow-xl w-72 sm:w-[30rem]">
+          <div :key="review.time" v-for="review in reviews" class="absolute item mx-4 rounded-lg bg-white border border-gray-200 shadow-lg hover:shadow-xl w-72 sm:w-[30rem]">
             <div class="p-4 sm:p-8">
               <blockquote>
                 <div>
                   <svg class="h-10 w-10 text-gb-600 opacity-25" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
                     <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                   </svg>
-                  <p id="reviewText" class="mt-6 text-sm sm:text-base font-medium text-gray-700">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  <p id="reviewText" class="mt-6 text-sm sm:text-base font-medium text-gray-700 line-clamp-6">
+                    {{ review.text }}
                   </p>
                 </div>
               </blockquote>
               <div class="mt-4">
                 <div class="inline-flex items-center">
                   <div class="flex-shrink-0 flex">
-                    <svg :key="rating" v-for="rating in 5" class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <svg :key="rating" v-for="rating in review.rating" class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                     </svg>
                   </div>
@@ -49,11 +49,11 @@
             </div>
             <div class="bottom-0 w-full py-3 px-5 sm:py-4 sm:px-6 rounded-b-lg flex items-center bg-gradient-to-br from-red-600 via-red-400 to-indigo-400">
               <div>
-                <img class="border-2 border-white w-10 h-10 flex-shrink-0 mx-auto bg-black rounded-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+                <img class="border-2 border-white w-10 h-10 flex-shrink-0 mx-auto bg-black rounded-full" :src="review.profile_photo_url" alt="">
               </div>
               <div class="pl-3">
-                <p class="font-black text-white leading-tight">Dan Nelson</p>
-                <p class="text-gray-200 text-sm leading-tight">2 days ago</p>
+                <p class="font-black text-white leading-tight">{{ review.author_name }}</p>
+                <p class="text-gray-200 text-sm leading-tight">{{ review.relative_time_description }}</p>
               </div>
             </div>
           </div>
@@ -63,6 +63,11 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import { Review } from '~/models/reviews'
+
+const { data: reviews, pending: loading, refresh, error } = await useLazyAsyncData<Review[]>('review', () => $fetch('/api/reviews', { method: 'GET', parseResponse: JSON.parse }))
+</script>
 
 <style>
 .item {
--- server/api/reviews.get.ts
+++ server/api/reviews.get.ts
@@ -3,13 +3,17 @@ import { ReviewApiResponse } from '~/models/reviews'
 const runtimeConfig = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
-  const response = await getReviews()
-  return JSON.stringify(response.result.reviews)
+  try {
+    const response = await getReviews()
+    return JSON.stringify(response.result.reviews)
+  } catch(error) {
+    console.log(error)
+    event.res.statusCode = 500
+    event.res.end()
+  }
 })
 
-const getReviews = async ():Promise<ReviewApiResponse> => {
-  const response:ReviewApiResponse = await $fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${runtimeConfig.googleBusinessId}&key=${runtimeConfig.googleKey}`, {
-    method: 'GET'
-  })
+const getReviews = async ():Promise<any> => {
+  const response = await $fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${runtimeConfig.googleBusinessId}&key=${runtimeConfig.googleKey}`)
   return response
 }
